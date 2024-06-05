847abf98bf07af91c309b58f157477a87951041b
Oscar A. Diaz <70609754+oscar1485@users.noreply.github.com>
2024-06-05 11:24:06 -0500

    actualización

--- appStreamlit - copia (3).py
+++ appStreamlit - copia (3).py
@@ -67,23 +67,31 @@ def main():
 
     elif choice == "Listar Aves Entrenadas":
         st.subheader("Listar Aves Entrenadas")
-        num_columns = 3
-        num_rows = int(np.ceil(len(names) / num_columns))
         birds_info = [
-            {"name": names[i], "image": f"static/imagen/{names[i].replace(' ', '_')}.jpg"} for i in range(len(names))
+            {"name": "CATHARTES+AURA", "image": "static/imagen/CATHARTES AURA_7.jpg"},
+            {"name": "COEREBA+FLAVEOLA", "image": "static/imagen/COEREBA FLAVEOLA.jpg"},
+            {"name": "COLUMBA+LIVIA", "image": "static/imagen/COLUMBA LIVIA_9.jpg"},
+            {"name": "CORAGYPS+ATRATUS", "image": "static/imagen/CORAGYPS ATRATUS_19.jpg"},
+            {"name": "CROTOPHAGA+SULCIROSTRIS", "image": "static/imagen/CROTOPHAGA SULCIROSTRIS_3.jpg"},
+            {"name": "CYANOCORAX+YNCAS", "image": "static/imagen/CYANOCORAX YNCAS_3.jpg"},
+            {"name": "EGRETTA+THULA", "image": "static/imagen/EGRETTA THULA_1.jpg"},
+            {"name": "FALCO+PEREGRINUS", "image": "static/imagen/FALCO PEREGRINUS_9.jpg"},
+            {"name": "FALCO+SPARVERIUS", "image": "static/imagen/FALCO SPARVERIUS_17.jpeg"},
+            {"name": "HIRUNDO+RUSTICA", "image": "static/imagen/HIRUNDO RUSTICA_10.jpg"},
+            {"name": "PANDION+HALIAETUS", "image": "static/imagen/PANDION HALIAETUS_5.jpg"},
+            {"name": "PILHERODIUS+PILEATUS", "image": "static/imagen/PILHERODIUS PILEATUS_14.jpeg"},
+            {"name": "PITANGUS+SULPHURATUS", "image": "static/imagen/PITANGUS SULPHURATUS_12.jpg"},
+            {"name": "PYRRHOMYIAS+CINNAMOMEUS", "image": "static/imagen/PYRRHOMYIAS CINNAMOMEUS_14.jpg"},
+            {"name": "RYNCHOPS+NIGER", "image": "static/imagen/RYNCHOPS NIGER_9.jpg"},
+            {"name": "SETOPHAGA+FUSCA", "image": "static/imagen/SETOPHAGA FUSCA_5.jpg"},
+            {"name": "SYNALLAXIS+AZARAE", "image": "static/imagen/SYNALLAXIS AZARAE_17.jpeg"},
+            {"name": "TYRANNUS+MELANCHOLICUS", "image": "static/imagen/TYRANNUS MELANCHOLICUS_12.jpg"},
         ]
 
-        for i in range(num_rows):
-            bird_row = birds_info[i * num_columns: (i + 1) * num_columns]
-            st.write('<div style="display: flex;">', unsafe_allow_html=True)
-            for bird in bird_row:
-                st.image(bird["image"], caption=bird["name"], width=100)
-            st.write('</div>', unsafe_allow_html=True)
-
-            st.write('<div style="display: flex;">', unsafe_allow_html=True)
-            for bird in bird_row:
-                st.markdown(f"[Buscar en Google](https://www.google.com/search?q={bird['name']})", unsafe_allow_html=True)
-            st.write('</div>', unsafe_allow_html=True)
+        for bird in birds_info:
+            st.image(bird["image"], caption=bird["name"], width=100)
+            st.markdown(f"[Buscar en Google](https://www.google.com/search?q={bird['name']})")
+            st.markdown("---")
 
 if __name__ == '__main__':
     main()

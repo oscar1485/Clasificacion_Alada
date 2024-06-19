0d0724297c76e2e51bebb938fcc983b332c0fe8c
Oscar A. Diaz <70609754+oscar1485@users.noreply.github.com>
2024-06-19 11:37:08 -0500

    Update appStreamlit.py

--- appStreamlit.py
+++ appStreamlit.py
@@ -120,7 +120,7 @@ def main():
         st.markdown("""
         Agradezco al Ministerio de Tecnologías de la Información y las Comunicaciones de Colombia por financiar la Maestría en Ciencia de Datos. 
         Asimismo, a la Universidad Cooperativa de Colombia Campus Ibagué - Espinal por facilitar el apoyo del tiempo dentro del Plan de Trabajo para realizar la Maestría. 
-        Además, a la Universidad Oberta de Cataluña por permitir la formación impartida y la materialización de las competencias aprendidas en este proyecto.
+        Además, a la Universidad Oberta de Cataluña por permitir la formación impartida y la materialización de las competencias aprendidas en este proyecto, a mis tutores Bernat Bas Pujols y Pablo Fernandez Blanco.
         """)
 
     # Item adicional al final

ed414bb0813ac50a5dc551cf40cb3025053c9e94
Oscar A. Diaz <70609754+oscar1485@users.noreply.github.com>
2024-06-26 12:55:24 -0500

    Update appStreamlit.py

--- appStreamlit.py
+++ appStreamlit.py
@@ -46,8 +46,8 @@ def load_bird_images(bird_name):
     
     # Mostrar la ruta que está siendo intentada
     st.write(f"Intentando acceder a la ruta: {bird_dir}")
-    bird_dir_buscar = os.path.join('datasetpreprocesado/test', bird_name.replace(' ', '+'))
-    st.markdown(f"[Buscar en Google](https://www.google.com/search?q={bird_dir_buscar})")
+   # bird_dir_buscar = os.path.join('datasetpreprocesado/test', bird_name.replace(' ', '+'))
+    st.markdown(f"[Buscar en Google](https://www.google.com/search?q=", bird_name.replace(' ', '+'))
     
     if os.path.exists(bird_dir):
         images = []

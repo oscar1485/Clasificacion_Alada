ea49def28e3e2acefd66b200a7ea1c3dded667d4
Oscar A. Diaz <70609754+oscar1485@users.noreply.github.com>
2024-07-12 15:32:54 -0500

    Update appStreamlit.py

--- appStreamlit.py
+++ appStreamlit.py
@@ -5,6 +5,7 @@ import streamlit as st
 from PIL import Image
 from skimage.transform import resize
 import pandas as pd
+import openpyxl
 import os
 
 # Path del modelo preentrenado
@@ -22,21 +23,6 @@ names = ['CATHARTES AURA', 'COEREBA FLAVEOLA', 'COLUMBA LIVIA', 'CORAGYPS ATRATU
          'EGRETTA THULA', 'FALCO PEREGRINUS', 'FALCO SPARVERIUS', 'HIRUNDO RUSTICA', 'PANDION HALIAETUS', 'PILHERODIUS PILEATUS',
          'PITANGUS SULPHURATUS', 'PYRRHOMYIAS CINNAMOMEUS', 'RYNCHOPS NIGER', 'SETOPHAGA FUSCA', 'SYNALLAXIS AZARAE', 'TYRANNUS MELANCHOLICUS']
 
-# Extensiones permitidas
-ALLOWED_EXTENSIONS = ('.png', '.jpg', '.jpeg')
-
-# Se carga el modelo al inicio
-@st.cache(allow_output_mutation=True)
-def load_keras_model(model_path):
-    try:
-        model = load_model(model_path)
-        return model
-    except Exception as e:
-        st.error(f"Error al cargar el modelo: {e}")
-        return None
-
-model = load_keras_model(MODEL_PATH)
-
 # Se recibe la imagen y el modelo, devuelve la predicción
 def model_prediction(img, model):
     img_resize = resize(img, (width_shape, height_shape))
@@ -50,12 +36,16 @@ def get_bird_info(bird_name, excel_path):
     df = pd.read_excel(excel_path)
     bird_info = df[df['Nombre_Cientifico'] == bird_name]
     if not bird_info.empty:
-        return bird_info.iloc[0]  # Selecciona la primera fila (debería ser única)
+        bird_info = bird_info.iloc[0]  # Selecciona la primera fila (debería ser única)
+        return bird_info
     else:
         return None
 
 def load_bird_images(bird_name):
-    bird_dir = os.path.join('datasetpreprocesado/test', bird_name.replace(' ', '_'))
+    bird_dir = os.path.join('datasetpreprocesado/test', bird_name.replace(' ', ' '))
+    
+    # Mostrar la ruta que está siendo intentada
+    #st.write(f"Intentando acceder a la ruta: {bird_dir}")
     
     bird_name_buscar = bird_name.replace(' ', '+')
     st.markdown(f"[Ver más Información](https://www.google.com/search?q={bird_name_buscar})")
@@ -63,7 +53,7 @@ def load_bird_images(bird_name):
     if os.path.exists(bird_dir):
         images = []
         for img_file in os.listdir(bird_dir):
-            if img_file.endswith(ALLOWED_EXTENSIONS):
+            if img_file.endswith(('png', 'jpg', 'jpeg')):
                 img_path = os.path.join(bird_dir, img_file)
                 images.append(img_path)
         return images
@@ -73,7 +63,6 @@ def load_bird_images(bird_name):
 def main():
     # Fondo o banner de la aplicación
     st.image("banner2.jpg", use_column_width=True)
-    
     st.title("Clasificación Alada")
     st.header("Sistema Multiclase para la Identificación Aviar en Ibagué")
     st.markdown("""
@@ -81,6 +70,7 @@ def main():
     El proyecto "Clasificación Alada" es un sistema multiclase diseñado para la identificación de aves en la región de Ibagué, 
     con un enfoque centrado en técnicas de Deep Learning. El objetivo principal es proporcionar una herramienta precisa y eficiente 
     para la clasificación de aves a partir de imágenes. Este trabajo fue apoyado  por el Ministerio de Tecnologías de la Información y las Comunicaciones de Colombia y la Universidad Cooperativa de Colombia, Campus Ibagué - Espinal.
+
     
     **Oscar Augusto Diaz Triana**  
     Universidad Oberta de Cataluña  
@@ -95,9 +85,15 @@ def main():
     menu = ["Información del Proyecto", "Realizar Predicciones", "Listar Aves Entrenadas", "Agradecimientos"]
     choice = st.sidebar.selectbox("Selecciona una opción", menu)
 
-    if model is None:
-        st.error("No se pudo cargar el modelo. Por favor, revisa el path y el archivo del modelo.")
+    # Se intenta cargar el modelo
+    try:
+        model = load_model(MODEL_PATH)
+        st.success("Modelo cargado correctamente")
+    except Exception as e:
+        st.error(f"Error al cargar el modelo: {e}")
         return
+    
+   
 
     if choice == "Realizar Predicciones":
         st.subheader("Realizar Predicciones")
@@ -155,12 +151,13 @@ def main():
             {"name": "PANDION+HALIAETUS", "image": "static/imagen/PANDION HALIAETUS_5.jpg"},
             {"name": "PILHERODIUS+PILEATUS", "image": "static/imagen/PILHERODIUS PILEATUS_14.jpeg"},
             {"name": "PITANGUS+SULPHURATUS", "image": "static/imagen/PITANGUS SULPHURATUS_12.jpg"},
-            {"name": "PYRRHOMYIAS+CINNAMOMEUS", "image": "static/imagen/PYRRHOMYIAS CINNAMOMEUS_4.jpg"},
-            {"name": "RYNCHOPS+NIGER", "image": "static/imagen/RYNCHOPS NIGER_15.jpeg"},
-            {"name": "SETOPHAGA+FUSCA", "image": "static/imagen/SETOPHAGA FUSCA_2.jpg"},
-            {"name": "SYNALLAXIS+AZARAE", "image": "static/imagen/SYNALLAXIS AZARAE_9.jpeg"},
-            {"name": "TYRANNUS+MELANCHOLICUS", "image": "static/imagen/TYRANNUS MELANCHOLICUS_17.jpeg"}
+            {"name": "PYRRHOMYIAS+CINNAMOMEUS", "image": "static/imagen/PYRRHOMYIAS CINNAMOMEUS_14.jpg"},
+            {"name": "RYNCHOPS+NIGER", "image": "static/imagen/RYNCHOPS NIGER_9.jpg"},
+            {"name": "SETOPHAGA+FUSCA", "image": "static/imagen/SETOPHAGA FUSCA_5.jpg"},
+            {"name": "SYNALLAXIS+AZARAE", "image": "static/imagen/SYNALLAXIS AZARAE_17.jpeg"},
+            {"name": "TYRANNUS+MELANCHOLICUS", "image": "static/imagen/TYRANNUS MELANCHOLICUS_12.jpg"},
         ]
+
         num_columns = 3
         num_rows = int(np.ceil(len(birds_info) / num_columns))
 
@@ -188,19 +185,30 @@ def main():
                         st.write(bird["name"])
                         st.markdown(f"[Buscar en Google](https://www.google.com/search?q={bird['name']})")
 
-            
     elif choice == "Agradecimientos":
-        st.header("Agradecimientos")
+        st.subheader("Agradecimientos")
         st.markdown("""
-        Este proyecto no habría sido posible sin el apoyo y la colaboración de muchas personas y organizaciones. 
-        Queremos expresar nuestro más sincero agradecimiento a:
-        
-        - **Universitat Oberta de Catalunya**: Por proporcionar una plataforma educativa excepcional y apoyo académico.
-        - **Ministerio de Tecnologías de la Información y las Comunicaciones de Colombia**: Por su apoyo financiero y logístico.
-        - **Universidad Cooperativa de Colombia, Campus Ibagué - Espinal**: Por su infraestructura y recursos para la investigación.
-        - **Bernat Bas Pujols y Pablo Fernandez Blanco**: Por su invaluable orientación y asesoría.
-        - **Albert Solé**: Por su liderazgo y enseñanza en la asignatura.
+        Agradezco al Ministerio de Tecnologías de la Información y las Comunicaciones de Colombia por financiar la Maestría en Ciencia de Datos. 
+        Asimismo, a la Universidad Cooperativa de Colombia Campus Ibagué - Espinal por facilitar el apoyo del tiempo dentro del Plan de Trabajo para realizar la Maestría. 
+        Además, a la Universidad Oberta de Cataluña por permitir la formación impartida y la materialización de las competencias aprendidas en este proyecto, a mis tutores Bernat Bas Pujols y Pablo Fernandez Blanco.
         """)
+
+     elif choice == "Información del Proyecto":
+        st.markdown("""
+    ### Información del Proyecto
+    El proyecto "Clasificación Alada" es un sistema multiclase diseñado para la identificación de aves en la región de Ibagué, 
+    con un enfoque centrado en técnicas de Deep Learning. El objetivo principal es proporcionar una herramienta precisa y eficiente 
+    para la clasificación de aves a partir de imágenes. Este trabajo fue apoyado  por el Ministerio de Tecnologías de la Información y las Comunicaciones de Colombia y la Universidad Cooperativa de Colombia, Campus Ibagué - Espinal.
+
+    
+    **Oscar Augusto Diaz Triana**  
+    Universidad Oberta de Cataluña  
+    Master en Ciencia de Datos  
+    Deep Learning  
+    **Tutores de TF:** Bernat Bas Pujols, Pablo Fernandez Blanco  
+    **Profesor responsable de la asignatura:** Albert Solé  
+    **2024**
+    """)
 
 if __name__ == '__main__':
     main()

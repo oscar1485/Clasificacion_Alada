5d57c27602cc8c3c92ee63968ec29dfa8b9d229d
Oscar A. Diaz <70609754+oscar1485@users.noreply.github.com>
2024-07-14 14:38:41 -0500

    Detectron2

--- appStreamlit.py
+++ appStreamlit.py
@@ -1,29 +1,62 @@
+import detectron2
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
 from keras.models import load_model
 from keras.applications.imagenet_utils import preprocess_input
 import numpy as np
+import cv2
+import torch
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog
+from detectron2 import model_zoo 
+
 import streamlit as st
 from PIL import Image
 from skimage.transform import resize
 import pandas as pd
-import openpyxl
 import os
 
-# Path del modelo preentrenado
-MODEL_PATH = 'models/optimizado.keras'
+# Definir clases de aves
+bird_classes = [14, 15, 16]  # Clases de aves en COCO dataset
 
-# Path del archivo Excel
+# Función para detectar aves en la imagen usando Detectron2
+def detect_birds(img):
+    # Convertir la imagen a RGB si tiene cuatro canales (RGBA)
+    if img.shape[2] == 4:
+        img = cv2.cvtColor(img, cv2.COLOR_RGBA2RGB)
+
+
+    # Crear la configuración y cargar el modelo preentrenado
+    cfg = get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.9  # Umbral de confianza para la detección
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+    cfg.MODEL.DEVICE = "cpu"  # Usar CPU en lugar de GPU
+    predictor = DefaultPredictor(cfg)
+    
+    # Realizar la predicción
+    outputs = predictor(img)
+    classes_detected = outputs["instances"].pred_classes.cpu().numpy()
+    
+    # Verificar si se detecta algún pájaro en la imagen
+    bird_detected = any(cls in bird_classes for cls in classes_detected)
+    
+    return bird_detected
+
+# Resto del código Streamlit
+MODEL_PATH = 'models/optimizado.keras'
 EXCEL_PATH = 'aves.xlsx'
 
-# Dimensiones de las imágenes de entrada    
 width_shape = 224
 height_shape = 224
 
-# Clases
 names = ['CATHARTES AURA', 'COEREBA FLAVEOLA', 'COLUMBA LIVIA', 'CORAGYPS ATRATUS', 'CROTOPHAGA SULCIROSTRIS', 'CYANOCORAX YNCAS',
          'EGRETTA THULA', 'FALCO PEREGRINUS', 'FALCO SPARVERIUS', 'HIRUNDO RUSTICA', 'PANDION HALIAETUS', 'PILHERODIUS PILEATUS',
          'PITANGUS SULPHURATUS', 'PYRRHOMYIAS CINNAMOMEUS', 'RYNCHOPS NIGER', 'SETOPHAGA FUSCA', 'SYNALLAXIS AZARAE', 'TYRANNUS MELANCHOLICUS']
 
-# Se recibe la imagen y el modelo, devuelve la predicción
 def model_prediction(img, model):
     img_resize = resize(img, (width_shape, height_shape))
     x = preprocess_input(img_resize * 255)
@@ -31,7 +64,6 @@ def model_prediction(img, model):
     preds = model.predict(x)
     return preds
 
-# Función para obtener información adicional del ave desde el archivo Excel
 def get_bird_info(bird_name, excel_path):
     df = pd.read_excel(excel_path)
     bird_info = df[df['Nombre_Cientifico'] == bird_name]
@@ -43,10 +75,6 @@ def get_bird_info(bird_name, excel_path):
 
 def load_bird_images(bird_name):
     bird_dir = os.path.join('datasetpreprocesado/test', bird_name.replace(' ', ' '))
-    
-    # Mostrar la ruta que está siendo intentada
-    #st.write(f"Intentando acceder a la ruta: {bird_dir}")
-    
     bird_name_buscar = bird_name.replace(' ', '+')
     st.markdown(f"[Ver más Información](https://www.google.com/search?q={bird_name_buscar})")
     
@@ -61,67 +89,65 @@ def load_bird_images(bird_name):
         return []
 
 def main():
-    # Se intenta cargar el modelo
     try:
         model = load_model(MODEL_PATH)
         st.success("Modelo cargado correctamente")
     except Exception as e:
         st.error(f"Error al cargar el modelo: {e}")
         return
-    # Fondo o banner de la aplicación
+
     st.image("banner2.jpg", use_column_width=True)
-    
     st.title("Clasificación Alada")
     st.header("Sistema Multiclase para la Identificación Aviar en Ibagué")
     
-
-    # Menú
     menu = ["Información del Proyecto", "Realizar Predicciones", "Listar Aves Entrenadas", "Agradecimientos"]
     choice = st.sidebar.selectbox("Selecciona una opción", menu)
-
-    
-    
-   
 
     if choice == "Realizar Predicciones":
         st.subheader("Realizar Predicciones")
         img_file_buffer = st.file_uploader("Carga una imagen", type=["png", "jpg", "jpeg"])
         
         if img_file_buffer is not None:
-            image = np.array(Image.open(img_file_buffer))    
+            image = np.array(Image.open(img_file_buffer))
             st.image(image, caption="Imagen", use_column_width=True)
-        
-        if st.button("Identificar Ave"):
-            if img_file_buffer is not None:
-                preds = model_prediction(image, model)
-                bird_name = names[np.argmax(preds)]
-                confidence = np.max(preds)
-                st.success(f'El ave es: {bird_name} con una precisión del {confidence:.2%}')
+            
+            # Validar si la imagen contiene aves usando Detectron2
+            bird_detected = detect_birds(image)
+            
+            if bird_detected:
+                if st.button("Identificar Ave"):
+                    preds = model_prediction(image, model)
+                    bird_name = names[np.argmax(preds)]
+                    confidence = np.max(preds)
+                    st.success(f'El ave es: {bird_name} con una precisión del {confidence:.2%}')
 
-                # Buscar información del ave en el archivo Excel
-                bird_info = get_bird_info(bird_name, EXCEL_PATH)
-                if bird_info is not None:
-                    st.write("**Nombre Científico:**", bird_info['Nombre_Cientifico'])
-                    st.write("**Nombre Común:**", bird_info['Nombre_Comun'])
-                    st.write("**Descripción General:**", bird_info['Descripcion_General'])
-                    st.write("**Distribución en el Tolima:**", bird_info['Distribucion_tolima'])
-                    st.write("**Distribución en Colombia:**", bird_info['Distribucion_Colombia'])
-                    st.write("**Estado de Conservación:**", bird_info['Estado_Conservacion'])
+                    bird_info = get_bird_info(bird_name, EXCEL_PATH)
+                    if bird_info is not None:
+                        st.write("**Nombre Científico:**", bird_info['Nombre_Cientifico'])
+                        st.write("**Nombre Común:**", bird_info['Nombre_Comun'])
+                        st.write("**Descripción General:**", bird_info['Descripcion_General'])
+                        st.write("**Distribución en el Tolima:**", bird_info['Distribucion_tolima'])
+                        st.write("**Distribución en Colombia:**", bird_info['Distribucion_Colombia'])
+                        st.write("**Estado de Conservación:**", bird_info['Estado_Conservacion'])
+                    else:
+                        st.warning("No se encontró información adicional sobre esta ave.")
+                    
+                    bird_images = load_bird_images(bird_name)
+                    if bird_images:
+                        st.subheader("Galería de Imágenes del Ave")
+                        cols = st.columns(3)
+                        for i, img_path in enumerate(bird_images):
+                            img = Image.open(img_path)
+                            cols[i % 3].image(img, use_column_width=True)
+                    else:
+                        st.warning("No se encontraron imágenes adicionales del ave en la galería.")
                 else:
-                    st.warning("No se encontró información adicional sobre esta ave.")
-                
-                # Cargar y mostrar imágenes del ave desde el directorio correspondiente
-                bird_images = load_bird_images(bird_name)
-                if bird_images:
-                    st.subheader("Galería de Imágenes del Ave")
-                    cols = st.columns(3)
-                    for i, img_path in enumerate(bird_images):
-                        img = Image.open(img_path)
-                        cols[i % 3].image(img, use_column_width=True)
-                else:
-                    st.warning("No se encontraron imágenes adicionales del ave en la galería.")
+                    st.warning("La imagen corresponde a un Ave, ya puedes dar clic en el botón para realizar la predicción.")
             else:
-                st.warning("Por favor, carga una imagen primero.")
+                st.warning("La imagen no contiene pájaros. No se puede realizar la predicción.Por favor, carga una imagen que contenga un ave.")
+        
+        else:
+            st.warning("Por favor, carga una imagen primero.")
 
     elif choice == "Listar Aves Entrenadas":
         st.subheader("Listar Aves Entrenadas")
@@ -152,11 +178,9 @@ def main():
         for i in range(num_rows):
             bird_row = birds_info[i * num_columns: (i + 1) * num_columns]
 
-            # Crear una fila de la tabla
             col1, col2, col3 = st.columns(3)
 
             for j, bird in enumerate(bird_row):
-                # Agregar imagen, nombre y enlace a Google a cada columna
                 if j == 0:
                     with col1:
                         st.image(bird["image"], caption=bird["name"], width=100)
